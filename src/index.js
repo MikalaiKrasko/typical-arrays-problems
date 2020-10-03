4d6ff36e82e68cd66f4027b0b1beda45b198774f
MikalaiKrasko <mikalai.krasko@gmail.com>
2020-10-03 16:15:43 +0300

    init: initial

---
+++ src/index.js
@@ -0,0 +1,42 @@
+
+exports.min = function min (array) {
+    if (array == 'undefined') {
+        return 0;
+    }
+    else {
+     var min = array[0];
+     for (var i = 1; i < array.length; i++) {
+      if (array[i] < min) {
+          min = array[i];
+      }
+        }
+    }
+  return min
+}
+
+exports.max = function max (array) {
+  if (array.length == 0)
+  return 0;
+  else
+ var max = array[0];
+   for (var i = 1; i < array.length; i++) {
+    if (array[i] > max) {
+        max = array[i];
+    }
+  }
+  return max
+}
+
+exports.avg = function avg (array) {
+  if (array.length == 0)
+  return 0;
+  else
+  var l = array.length;
+  var sum = array[0];
+  for (var i = 1; i < array.length; i++) {
+      sum = sum + array[i];
+  }
+  var a=sum/l;
+return a;
+}
+
